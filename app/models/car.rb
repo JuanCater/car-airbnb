a5df9ea81f690bd509d343319bc6d8a22e9335c4
Sam Musler <sam.a.musler@gmail.com>
2023-03-14 12:12:02 -0300

    cloudinary and car user id assigned

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,7 +4,7 @@ class Car < ApplicationRecord
   # has_many :reservations
   # has_many :users, through: :reservations
 
-  has_many_attached :photo
+  has_many_attached :photos
   validates :year, presence: true
   validates :model, presence: true
   validates :brand, presence: true
