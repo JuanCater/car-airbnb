cb196944bdd2fe9e9808e24bc36f700c9cc4722d
Isabelle Brown <bellebrown619@gmail.com>
2023-03-16 10:18:51 -0400

    dependents

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,7 +4,7 @@ class Car < ApplicationRecord
 
   belongs_to :user
 
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations
 
   has_many_attached :photos
